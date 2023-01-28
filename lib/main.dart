d449bac043711af74cff83faaafe3ebdb69bb790
AbhinavHaridas <abhikeabhi2002@gmail.com>
2023-01-29 01:31:54 +0530

    Completed one screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,57 +15,102 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.black,
-          body: SingleChildScrollView(
-           scrollDirection: Axis.vertical,
-           child: Column(
-             children: [
-               Container(
-                 color: Colors.white10,
-                 margin: const EdgeInsets.only(
-                     top: 60.0
-                 ),
-                 padding: const EdgeInsets.only(
-                   top: 30.0,
-                   bottom: 30.0,
-                 ),
-                 child: Column(
-                   children: [
-                     Column(
-                       // Featured Today Section
-                       children: const [
-                         Heading(title: "Featured today"),
-                         FeaturedToday()
-                       ],
-                     ),
-                   ],
-                 ),
-               ),
-               Container(
-                 color: Colors.white10,
-                 padding: const EdgeInsets.only(
-                     top: 30.0,
-                     bottom: 30.0
-                 ),
-                 child: Column(
-                   children: const [
-                     Heading(title: "What to watch"),
-                     Suggestions()
-                   ],
-                 ),
-               )
-             ],
-           ),
-          ),
-          bottomNavigationBar: const BottomNavbar(),
-          ),
-          );
+        home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.white10,
+              margin: const EdgeInsets.only(
+                  top: 60.0
+              ),
+              padding: const EdgeInsets.only(
+                top: 30.0,
+                bottom: 30.0,
+              ),
+              child: Column(
+                children: [
+                  Column(
+                    // Featured Today Section
+                    children: const [
+                      Heading(title: "Featured today"),
+                      FeaturedToday()
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white10,
+              padding: const EdgeInsets.only(
+                  top: 30.0,
+                  bottom: 30.0
+              ),
+              child: Column(
+                children: const [
+                  Heading(title: "What to watch"),
+                  Suggestions(
+                    title: "From your Watchlist",
+                    description: "Shows and movies available to watch from your watch list",
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white10,
+              margin: const EdgeInsets.only(
+                  top: 15.0
+              ),
+              padding: const EdgeInsets.only(
+                  bottom: 30.0
+              ),
+              child: const Suggestions(
+                  title: "Top picks for you",
+                  description: "TV shows and movies just for you"
+              ),
+            ),
+            Container(
+              color: Colors.white10,
+              margin: const EdgeInsets.only(
+                  top: 10.0
+              ),
+              padding: const EdgeInsets.only(
+                  bottom: 30.0
+              ),
+              child: const Suggestions(
+                  title: "Fan favourites",
+                  description: "This week's top TV shows and movies"
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: const BottomNavbar(),
+    );
   }
 }
 
 class Suggestions extends StatelessWidget {
-  const Suggestions({Key? key}) : super(key: key);
+  const Suggestions({
+      required this.title,
+      required this.description,
+      super.key
+  });
+
+  final String title;
+  final String description;
 
   @override
   Widget build(BuildContext context) {
@@ -82,28 +127,30 @@ class Suggestions extends StatelessWidget {
                 padding: const EdgeInsets.only(
                     left: 10.0
                 ),
-                child: SizedBox(
-                  width: 13.0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(3),
-                          color: Colors.amberAccent,
-                        ),
-                       width: 5.0,
-                        height: 20.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3),
+                        color: Colors.amberAccent,
                       ),
-                      const Text(
-                          "From your Watchlist",
-                          style: TextStyle(
+                     width: 5.0,
+                      height: 20.0,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(
+                        left: 13.0,
+                      ),
+                      child: Text(
+                          title,
+                          style: const TextStyle(
                             fontSize: 20.0,
                             color: Colors.white
                           ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
              Container(
@@ -124,17 +171,233 @@ class Suggestions extends StatelessWidget {
         ),
         Container(
           margin: const EdgeInsets.only(
-            top: 10.0
+            top: 13.0
           ),
-          child: const Text(
-              "Shows and movies available to watch from your watch list",
-              style: TextStyle(
-                fontSize: 14.0,
+          child: Text(
+              description,
+              style: const TextStyle(
+                fontSize: 15.0,
                 color: Colors.grey
               ),
+              textAlign: TextAlign.end,
           ),
-        )
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+              children: const [
+                SmallInfo(
+                    info1: "Movie",
+                    info2: "TV Series"
+                ),
+                SmallInfo(
+                    info1: "Prime Video(Rent/Buy)",
+                    info2:  "Other Providers"
+                ),
+                SmallInfo(
+                    info1: "Drama",
+                    info2: "Adventure"
+                )
+              ],
+          ),
+        ),
+        const MovieList(),
       ],
+    );
+  }
+}
+
+class MovieItem extends StatelessWidget {
+  const MovieItem({
+    required this.name,
+    required this.link,
+    required this.rating,
+    required this.year,
+    required this.age,
+    required this.time,
+    super.key
+  });
+
+  final String name;
+  final String link;
+  final String rating;
+  final String year;
+  final String age;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+       margin: const EdgeInsets.only(
+           left: 12.0
+       ),
+       child: Column(
+         children: [
+           Image(
+               image: ResizeImage(
+                 AssetImage(link),
+                 width: 100,
+                 height: 150,
+               )
+           ),
+           SizedBox(
+                width: 200,
+               child: Column(
+                children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 12),
+                      child: Text(
+                        rating,
+                        style: const TextStyle(
+                            fontSize: 15.0
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 12),
+                      child: Text(
+                        name,
+                        style: const TextStyle(
+                          fontSize: 15.0
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(year, style: const TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.grey
+                          ),),
+                          Text(age, style: const TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.grey
+                          ),),
+                          Text(time, style: const TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.grey
+                          ),)
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(12.0),
+                      margin: const EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                          )
+                      ),
+                      child: const Text(
+                        "Watch options",
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue
+                        ),
+                      ),
+                    )
+                  ],
+             )
+           )
+         ],
+       )
+   );
+  }
+}
+
+class MovieList extends StatelessWidget {
+  const MovieList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: const [
+          MovieItem(
+              name: "Catch Me If You Can",
+              link: "movieList/catch_me.jpg",
+              rating: "10.0",
+              year: "2002",
+              age: "PG-13",
+              time: "2hrs"
+          ),
+          MovieItem(
+              name: "God Father",
+              link: "movieList/godfather.jpg",
+              rating: "10.0",
+              year: "1992",
+              age: "R",
+              time: "2.3hrs"
+          ),
+          MovieItem(
+              name: "good fellas",
+              link: "movieList/good fellas.jpg",
+              rating: "9.0",
+              year: "1999",
+              age: "R",
+              time: "2.5hrs"
+          ),
+          MovieItem(
+              name: "The Social Network",
+              link: "movieList/social_network.jpg",
+              rating: "10.0",
+              year: "2012",
+              age: "R",
+              time: "2hrs"
+          ),
+          MovieItem(
+              name: "Catch Me If You Can",
+              link: "movieList/all at once.jpg",
+              rating: "9.0",
+              year: "2022",
+              age: "PG-13",
+              time: "2hrs"
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SmallInfo extends StatelessWidget {
+  const SmallInfo({required this.info1, required this.info2, super.key});
+
+  final String info1;
+  final String info2;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Row(
+       children: [
+         Container(
+           margin: const EdgeInsets.all(15.0),
+           padding: const EdgeInsets.all(10.0),
+           decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey
+              ),
+              borderRadius: BorderRadius.circular(
+                20.0
+              )
+           ),
+           child: Row(
+             children:  [
+               Text(info1),
+               const VerticalDivider(
+                 color: Colors.grey,
+                 width: 21,
+               ),
+               Text(info2)
+             ],
+           ),
+         )
+       ],
+      )
     );
   }
 }
@@ -263,12 +526,21 @@ class Inside extends StatelessWidget {
            Padding(
             padding: const EdgeInsets.only(left: 10.0, top: 15.0),
             child: Image(
-              image: AssetImage(image),
+              image: ResizeImage(
+                AssetImage(image),
+                width: 400,
+                height: 220
+              ),
             ),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 5.0),
-            child: Text(title),
+            child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 15.0
+                ),
+            ),
           ),
         ],
       ),
